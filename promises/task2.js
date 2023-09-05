78dc64361f3d68019d7354eb73bc498adeb7f794
Francesca Pavan <francescapavan7620@gmail.com>
2023-09-05 09:37:10 -0300

    Promises task 2 correction

--- promises/task2.js
+++ promises/task2.js
@@ -1,23 +1,50 @@
 function promiseAllSettled(promises) {
-    return Promise.all(promises.map((promise) => {
-      return promise
-        .then((value) => ({ status: 'fulfilled', value }))
-        .catch((reason) => ({ status: 'rejected', reason }));
-    }));
+  const settledPromises = [];
+  
+  function settlePromise(promise, index) {
+    promise
+      .then((value) => {
+        settledPromises[index] = { status: 'fulfilled', value };
+      })
+      .catch((reason) => {
+        settledPromises[index] = { status: 'rejected', reason };
+      })
+      .finally(() => {
+        // Check if all promises have settled
+        if (settledPromises.length === promises.length) {
+          return settledPromises;
+        }
+      });
   }
   
-  // Example usage:
-  const promises = [
-    Promise.resolve(1),
-    Promise.reject("Error occurred"),
-    Promise.resolve(3),
-  ];
-  
-  promiseAllSettled(promises)
-    .then((results) => {
-      console.log("All promises settled:", results);
-      // Expected: [{ status: 'fulfilled', value: 1 },
-      //            { status: 'rejected', reason: 'Error occurred' },
-      //            { status: 'fulfilled', value: 3 }]
-    });
-  
+  for (let i = 0; i < promises.length; i++) {
+    settlePromise(promises[i], i);
+  }
+
+  return new Promise((resolve) => {
+    const checkAllPromisesSettled = () => {
+      if (settledPromises.length === promises.length) {
+        resolve(settledPromises);
+      } else {
+        setTimeout(checkAllPromisesSettled, 0);
+      }
+    };
+    
+    checkAllPromisesSettled();
+  });
+}
+
+// Example usage:
+const promises = [
+  Promise.resolve(1),
+  Promise.reject("Error occurred"),
+  Promise.resolve(3),
+];
+
+promiseAllSettled(promises)
+  .then((results) => {
+    console.log("All promises settled:", results);
+    // Expected: [{ status: 'fulfilled', value: 1 },
+    //            { status: 'rejected', reason: 'Error occurred' },
+    //            { status: 'fulfilled', value: 3 }]
+  });
