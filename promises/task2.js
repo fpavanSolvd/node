58e2424c269cae9e501f14a0ce1fcdc24492d6c9
Francesca Pavan <francescapavan7620@gmail.com>
2023-09-05 10:04:02 -0300

    Promises task 2 correction 2.0

--- promises/task2.js
+++ promises/task2.js
@@ -1,50 +1,51 @@
 function promiseAllSettled(promises) {
   const settledPromises = [];
-  
-  function settlePromise(promise, index) {
-    promise
-      .then((value) => {
-        settledPromises[index] = { status: 'fulfilled', value };
-      })
-      .catch((reason) => {
-        settledPromises[index] = { status: 'rejected', reason };
-      })
-      .finally(() => {
-        // Check if all promises have settled
-        if (settledPromises.length === promises.length) {
-          return settledPromises;
-        }
-      });
-  }
-  
-  for (let i = 0; i < promises.length; i++) {
-    settlePromise(promises[i], i);
-  }
+  let settledCount = 0;
 
   return new Promise((resolve) => {
-    const checkAllPromisesSettled = () => {
-      if (settledPromises.length === promises.length) {
-        resolve(settledPromises);
-      } else {
-        setTimeout(checkAllPromisesSettled, 0);
-      }
-    };
-    
-    checkAllPromisesSettled();
+    if (promises.length === 0) {
+      resolve(settledPromises);
+      return;
+    }
+
+    function settlePromise(promise, index) {
+      promise
+        .then((value) => {
+          settledPromises[index] = { status: 'fulfilled', value };
+          settledCount++;
+
+          if (settledCount === promises.length) {
+            resolve(settledPromises);
+          }
+        })
+        .catch((reason) => {
+          settledPromises[index] = { status: 'rejected', reason };
+          settledCount++;
+
+          if (settledCount === promises.length) {
+            resolve(settledPromises);
+          }
+        });
+    }
+
+    for (let i = 0; i < promises.length; i++) {
+      settlePromise(promises[i], i);
+    }
   });
 }
 
 // Example usage:
-const promises = [
-  Promise.resolve(1),
-  Promise.reject("Error occurred"),
-  Promise.resolve(3),
-];
-
-promiseAllSettled(promises)
-  .then((results) => {
-    console.log("All promises settled:", results);
-    // Expected: [{ status: 'fulfilled', value: 1 },
-    //            { status: 'rejected', reason: 'Error occurred' },
-    //            { status: 'fulfilled', value: 3 }]
-  });
+const promise1 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('Resolved after 100ms');
+  }, 100);
+});
+const promise2 = new Promise((_, reject) => {
+  setTimeout(() => {
+    reject('Rejected after 50ms');
+  }, 50);
+});
+const promises = [promise1, promise2];
+promiseAllSettled(promises).then((results) => {
+  console.log(results);
+});
